a230692691d0d5e4d36179d3233a524638555770
satriobismaa <bismazharfan@gmail.com>
2026-04-30 18:35:51 +0700

    menambahkan landing page

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from main.views import navbar, dashboard, profil_view
+from main.views import landing, dashboard, profil_view
 
 app_name = "main"
 
 urlpatterns = [
-    path("", navbar, name="navbar"),
+    path("", landing, name="landing"),
     path("dashboard/", dashboard, name="dashboard"),
-    path('pengaturan-profil/', profil_view, name='profil'),
+    path("pengaturan-profil/", profil_view, name="profil"),
 ]
